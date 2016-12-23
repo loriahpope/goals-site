11979757cb8ded990ce85b04ec6f8d7c84fd8261
Loriah Pope <loriahpope@Loriahs-MBP.attlocal.net>
2016-12-23 16:17:10 -0600

    replace with contacts app

--- public/js/app.js
+++ public/js/app.js
@@ -1,125 +1,116 @@
-angular.module("goalsApp", ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when("/", {
-        templateUrl: "list.html",
-        controller: "ListController",
-        resolve: {
-          goals: function(Goals) {
-              return Goals.getGoals();
-          }
+angular.module("contactsApp", ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "list.html",
+                controller: "ListController",
+                resolve: {
+                    contacts: function(Contacts) {
+                        return Contacts.getContacts();
+                    }
+                }
+            })
+            .when("/new/contact", {
+                controller: "NewContactController",
+                templateUrl: "contact-form.html"
+            })
+            .when("/contact/:contactId", {
+                controller: "EditContactController",
+                templateUrl: "contact.html"
+            })
+            .otherwise({
+                redirectTo: "/"
+            })
+    })
+    .service("Contacts", function($http) {
+        this.getContacts = function() {
+            return $http.get("/contacts").
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error finding contacts.");
+                });
         }
-      })
-      .when("/new/goal", {
-        templateUrl: "goal-form.html",
-        controller: "NewGoalController"
-      })
-      .when("/goal/:goalId", {
-        controller: "EditGoalController",
-        templateUrl: "goal.html"
-      })
-      .otherwise({
-        redirectTo: "/"
-      })
-  })
-  .service("Goals", function($http) {
-    this.getGoals = function() {
-      return $http.get("/goals")
-        .then(function(response) {
-            return response;
-        }, function(response) {
-            alert("Error retrieving goals.");
-        });
-    }
-    this.createGoal = function(goal) {
-      return $http.post("/goals", goal)
-        .then(function(response) {
-            return response;
-        }, function(response) {
-            alert("Error creating goal.");
-        });
-    }
-    this.getGoal = function(goalId) {
-      var url = "/goals/" + goalId;
-      return $http.get(url)
-      .then(function(response) {
-        return response;
-      }, function(response) {
-        alert("Error finding this goal.");
-      })
-    }
-    this.editGoal = function(goal) {
-      var url = "/goals/" + goal._id;
-      console.log(goal._id);
-      return $http.put(url, goal).
-          then(function(response) {
-              return response;
-          }, function(response) {
-              alert("Error editing this goal.");
-              console.log(response);
-          });
-    }
-    this.deleteGoal = function(goalId) {
-      var url = "/goals/" + goalId;
-      return $http.delete(url).
-          then(function(response) {
-              return response;
-          }, function(response) {
-              alert("Error deleting this goal.");
-              console.log(response);
-          });
-    }
-  })
-  .controller("ListController", function(goals, $scope) {
-    $scope.goals = goals.data;
-  })
-  .controller("NewGoalController", function($scope, $location, Contacts) {
-    $scope.back = function() {
-        $location.path("#/");
-    }
+        this.createContact = function(contact) {
+            return $http.post("/contacts", contact).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error creating contact.");
+                });
+        }
+        this.getContact = function(contactId) {
+            var url = "/contacts/" + contactId;
+            return $http.get(url).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error finding this contact.");
+                });
+        }
+        this.editContact = function(contact) {
+            var url = "/contacts/" + contact._id;
+            console.log(contact._id);
+            return $http.put(url, contact).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error editing this contact.");
+                    console.log(response);
+                });
+        }
+        this.deleteContact = function(contactId) {
+            var url = "/contacts/" + contactId;
+            return $http.delete(url).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error deleting this contact.");
+                    console.log(response);
+                });
+        }
+    })
+    .controller("ListController", function(contacts, $scope) {
+        $scope.contacts = contacts.data;
+    })
+    .controller("NewContactController", function($scope, $location, Contacts) {
+        $scope.back = function() {
+            $location.path("#/");
+        }
 
-    $scope.saveGoal = function(goal) {
-        Goals.createGoal(goal).then(function(doc) {
-            var goalUrl = "/goal/" + doc.data._id;
-            $location.path(goalUrl);
-        }, function(response) {
-            alert(response);
-        });
-    }
-  })
-  .controller("EditGoalController", function($scope, $routeParams, Goals) {
-        Goals.getGoal($routeParams.goalsId).then(function(doc) {
-            $scope.goal = doc.data;
+        $scope.saveContact = function(contact) {
+            Contacts.createContact(contact).then(function(doc) {
+                var contactUrl = "/contact/" + doc.data._id;
+                $location.path(contactUrl);
+            }, function(response) {
+                alert(response);
+            });
+        }
+    })
+    .controller("EditContactController", function($scope, $routeParams, Contacts) {
+        Contacts.getContact($routeParams.contactId).then(function(doc) {
+            $scope.contact = doc.data;
         }, function(response) {
             alert(response);
         });
 
         $scope.toggleEdit = function() {
             $scope.editMode = true;
-            $scope.goalFormUrl = "goal-form.html";
+            $scope.contactFormUrl = "contact-form.html";
         }
 
         $scope.back = function() {
             $scope.editMode = false;
-            $scope.goalFormUrl = "";
+            $scope.contactFormUrl = "";
         }
 
-        $scope.saveGoal = function(goal) {
-            Goals.editGoal(goal);
+        $scope.saveContact = function(contact) {
+            Contacts.editContact(contact);
             $scope.editMode = false;
-            $scope.goalFormUrl = "";
+            $scope.contactFormUrl = "";
         }
 
-        $scope.deleteGoal = function(goalId) {
-            Goals.deleteGoal(goalId);
+        $scope.deleteContact = function(contactId) {
+            Contacts.deleteContact(contactId);
         }
     });
-
-
-
-
-
-
-
-
-
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var GOALS_COLLECTION = "goals";
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-// Connect to the database before starting the application server.
+// Connect to the database before starting the application server. 
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
@@ -39,95 +39,73 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/* "/goals"
- * GET: finds all goals
- * POST: creates a new goal
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
  */
 
-
-/* Schema for goals
- * Goals must be (S)pecific, (M)easurable, (A)ttainable, (R)ealistic, and (T)ime-Based
- * What do you want to acheive? (ex: Run a Marathon)
- * When do you want to acheive it by? (ex: July 2017)
- * What are 3 steps you will take to acheive this goal? (+ Dates)
- * How will you know you have succeeded? (ex: I will run a Marathon in under 3.5 hours) 
- * 
- * {
- *    "_id": <ObjectId>,
- *    "createDate": <Date>,
- *    "goalName": <string>,
- *    "goalDate": <Date>,
- *    "goalSteps": {
- *        "stepOne": <string>,
- *        "stepTwo": <string>, 
- *        "stepThree": <string>
- *    },
- *    "goalStatement": <string>
- * }
- */ 
-
-app.get("/goals", function(req, res) {
-  db.collection(GOALS_COLLECTION).find({}).toArray(function(err, docs) {
-    if(err) {
-      handleError(res, err.message, "Failed to get goals.");
+app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
     } else {
-      res.status(200).json(docs);
+      res.status(200).json(docs);  
     }
-  })
+  });
 });
 
-app.post("/goals", function(req, res) {
-  var newGoal = req.body;
-  newGoal.createDate = new Date();
+app.post("/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
 
-  if(!(req.body.goalName)) {
-    handleError(res, "Invalid user input", "Must provide a goal name", 400);
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
   }
 
-  db.collection(GOALS_COLLECTION).insertOne(newGoal, function(err, doc) {
-    if(err) {
-      handleError(res, err.message, "Failed to create new goal.");
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
     } else {
       res.status(201).json(doc.ops[0]);
     }
-  })
+  });
 });
 
-/* "/goals/:id"
- * GET: find goal by id
- * PUT: update goal by id
- * DELETE: deletes goal by id
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
  */
 
-app.get("/goals/:id", function(req,res) {
-  db.collection(GOALS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function(err, doc) {
-    if(err) {
-      handleError(res, err.message, "Failed to get goal.");
+app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
     } else {
-      res.status(200).json(doc);
+      res.status(200).json(doc);  
     }
-  })
+  });
 });
 
-app.put("/goals/:id", function(req,res) {
+app.put("/contacts/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(GOALS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if(err) {
-      handleError(res, err.message, "Failed to update goal.");
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update contact");
     } else {
       res.status(204).end();
     }
-  })
+  });
 });
 
-app.delete("/goals/:id", function(req,res) {
-  db.collection(GOALS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    if(err) {
-      handleError(res, err.message, "Failed to delete goal.");
+app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
     } else {
       res.status(204).end();
     }
-  })
+  });
 });
