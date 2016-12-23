f7312caf8669ee8631676583cd8c24cd9c5bac6d
Loriah Pope <loriahpope@Loriahs-MBP.attlocal.net>
2016-12-23 16:13:21 -0600

    routes

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,13 @@ angular.module("goalsApp", ['ngRoute'])
         templateUrl: "goal-form.html",
         controller: "NewGoalController"
       })
+      .when("/goal/:goalId", {
+        controller: "EditGoalController",
+        templateUrl: "goal.html"
+      })
+      .otherwise({
+        redirectTo: "/"
+      })
   })
   .service("Goals", function($http) {
     this.getGoals = function() {
@@ -32,6 +39,36 @@ angular.module("goalsApp", ['ngRoute'])
             alert("Error creating goal.");
         });
     }
+    this.getGoal = function(goalId) {
+      var url = "/goals/" + goalId;
+      return $http.get(url)
+      .then(function(response) {
+        return response;
+      }, function(response) {
+        alert("Error finding this goal.");
+      })
+    }
+    this.editGoal = function(goal) {
+      var url = "/goals/" + goal._id;
+      console.log(goal._id);
+      return $http.put(url, goal).
+          then(function(response) {
+              return response;
+          }, function(response) {
+              alert("Error editing this goal.");
+              console.log(response);
+          });
+    }
+    this.deleteGoal = function(goalId) {
+      var url = "/goals/" + goalId;
+      return $http.delete(url).
+          then(function(response) {
+              return response;
+          }, function(response) {
+              alert("Error deleting this goal.");
+              console.log(response);
+          });
+    }
   })
   .controller("ListController", function(goals, $scope) {
     $scope.goals = goals.data;
@@ -49,4 +86,40 @@ angular.module("goalsApp", ['ngRoute'])
             alert(response);
         });
     }
-})
+  })
+  .controller("EditGoalController", function($scope, $routeParams, Goals) {
+        Goals.getGoal($routeParams.goalsId).then(function(doc) {
+            $scope.goal = doc.data;
+        }, function(response) {
+            alert(response);
+        });
+
+        $scope.toggleEdit = function() {
+            $scope.editMode = true;
+            $scope.goalFormUrl = "goal-form.html";
+        }
+
+        $scope.back = function() {
+            $scope.editMode = false;
+            $scope.goalFormUrl = "";
+        }
+
+        $scope.saveGoal = function(goal) {
+            Goals.editGoal(goal);
+            $scope.editMode = false;
+            $scope.goalFormUrl = "";
+        }
+
+        $scope.deleteGoal = function(goalId) {
+            Goals.deleteGoal(goalId);
+        }
+    });
+
+
+
+
+
+
+
+
+
