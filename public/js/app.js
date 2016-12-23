b7f1f2b78a607c0b417e99a883e0a5b3c57b9c1a
Loriah Pope <loriahpope@Loriahs-MBP.attlocal.net>
2016-12-23 15:48:41 -0600

    adding new goal form

--- public/js/app.js
+++ public/js/app.js
@@ -10,17 +10,43 @@ angular.module("goalsApp", ['ngRoute'])
           }
         }
       })
+      .when("/new/goal", {
+        templateUrl: "goal-form.html",
+        controller: "NewGoalController"
+      })
   })
   .service("Goals", function($http) {
     this.getGoals = function() {
-      return $http.get("/goals").
-        then(function(response) {
+      return $http.get("/goals")
+        .then(function(response) {
             return response;
         }, function(response) {
             alert("Error retrieving goals.");
         });
     }
+    this.createGoal = function(goal) {
+      return $http.post("/goals", goal)
+        .then(function(response) {
+            return response;
+        }, function(response) {
+            alert("Error creating goal.");
+        });
+    }
   })
   .controller("ListController", function(goals, $scope) {
     $scope.goals = goals.data;
-  });
+  })
+  .controller("NewGoalController", function($scope, $location, Contacts) {
+    $scope.back = function() {
+        $location.path("#/");
+    }
+
+    $scope.saveGoal = function(goal) {
+        Goals.createGoal(goal).then(function(doc) {
+            var goalUrl = "/goal/" + doc.data._id;
+            $location.path(goalUrl);
+        }, function(response) {
+            alert(response);
+        });
+    }
+})
